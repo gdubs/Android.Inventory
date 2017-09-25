64b036e3aa1b55f0cc87e5a942fabf61b99593ff
gdubs <gdubs.social@gmail.com>
2017-09-25 15:00:26 +0800

    rm
    removed

--- SimpleInventory/app/src/main/java/simpleinventory/simpleinventory/interfaces/FragmentActivityCommunicator.java
+++
@@ -1,8 +0,0 @@
-package simpleinventory.simpleinventory.interfaces;
-
-/**
- * Created by gwulfwud on 8/5/2016.
- */
-public interface FragmentActivityCommunicator {
-    public void showSnackBarMessage(String message);
-}
